d63eabcb93a9c8a4d791ae126b075b64026a96f2
Janos Gyerik <janos@xw8400>
2013-11-17 22:26:16 +0100

    echo vim -d in addition to diff

--- compare.sh
+++ compare.sh
@@ -4,6 +4,6 @@ cd $(dirname "$0")
 
 for i in .??*; do
     if test -f ~/$i; then
-        cmp $i ~/$i >/dev/null || echo diff $i ~/$i
+        cmp $i ~/$i >/dev/null || { echo diff $i ~/$i; echo vim -d $i ~/$i; }
     fi
 done
